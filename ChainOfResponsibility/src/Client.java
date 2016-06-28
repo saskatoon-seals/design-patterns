e209dd4bb5cecc44fb3213e28f3f1652a8e9faef
Ales Ursic <saskatoon.seals@gmail.com>
2016-06-27 19:30:17 -0600

    Testing GitHub and EGit integration

--- ChainOfResponsibility/src/Client.java
+++ ChainOfResponsibility/src/Client.java
@@ -5,6 +5,10 @@ public class Client {
 		RequestHandler firstRequestHandler = new RequestHandlerA();
 		RequestHandler secondRequestHandler = new RequestHandlerB();
 		firstRequestHandler.setNextRequestHandler(secondRequestHandler);
+		
+		//Added a new line just to see if change set can be seen both on GitHub and 
+		//EGit if a pull request is created
+		System.out.println("Testing GitHub");
 
 		firstRequestHandler.handleRequest(new Request("Ales", -1));
 		firstRequestHandler.handleRequest(new Request("Maja", 5));
