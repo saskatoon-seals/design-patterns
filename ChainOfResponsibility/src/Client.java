371ab07715e83a5cee42490fccccac48de3e7e28
Ales Ursic <saskatoon.seals@gmail.com>
2016-07-01 13:15:56 -0600

    Commit 2: Even more new testing stuff - appraise

--- ChainOfResponsibility/src/Client.java
+++ ChainOfResponsibility/src/Client.java
@@ -9,6 +9,8 @@ public class Client {
 		//Added a new line just to see if change set can be seen both on GitHub and 
 		//EGit if a pull request is created
 		System.out.println("Testing GitHub");
+		
+		//Even more new stuff
 
 		firstRequestHandler.handleRequest(new Request("Ales", -1));
 		firstRequestHandler.handleRequest(new Request("Maja", 5));
